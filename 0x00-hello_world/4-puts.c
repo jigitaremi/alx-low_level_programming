526598bccb81eeba1cd82504ee5315003c48af0a
Oluwaremilekun <simplyremmy@gmail.com>
2023-02-19 00:06:21 -0800

    Writing a C program using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * The main function of the program
+ * This function is called when the program starts.
+ * It prints a string using the puts function
+ *
+ * @return The exit status of the program.
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
