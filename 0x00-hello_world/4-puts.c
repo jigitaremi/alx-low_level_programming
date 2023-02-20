3dd5c8f0e80070a2a728130b1538f4870c179f41
Oluwaremilekun <simplyremmy@gmail.com>
2023-02-19 17:04:45 -0800

    A C program that prints using the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * The main function of the program
- * This function is called when the program starts.
- * It prints a string using the puts function
+ * main - Entry point
  *
- * @return The exit status of the program.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts($"Programming is like building a multilingual puzzle);
 	return (0);
 }
