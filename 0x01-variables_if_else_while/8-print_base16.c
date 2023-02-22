821887ccd7a43063a4030e84c26a27f4c2a2f198
Oluwaremilekun <simplyremmy@gmail.com>
2023-02-22 14:19:43 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+		for (j = 'a'; j <= 'f'; j++)
+		{
+			putchar(j);
+		}
+	putchar('\n');
+	return (0);
+}
