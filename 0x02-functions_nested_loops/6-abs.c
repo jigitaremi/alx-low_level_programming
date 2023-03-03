78491b009e022b482872cdb6f40cb4ecc55d9b63
Oluwaremilekun <simplyremmy@gmail.com>
2023-03-03 06:41:07 -0800

    abs checking

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - function to print absolute of an integer
+ * @n: the number to be checked
+ *
+ * Return: 0 if success
+ */
+int _abs(int n)
+{
+	int i = (abs(n));
+
+	return (i);
+}
