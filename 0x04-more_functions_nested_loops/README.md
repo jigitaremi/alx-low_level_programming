3778093e426564f62b0edbf1fb4b1e89a3a918f1
Oluwaremilekun <simplyremmy@gmail.com>
2023-03-05 12:53:31 -0800

    more nested loops

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# Readme file for learning nested loops and functions 
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - displays the character c
+ * @c: the character to be printed
+ *
+ * Return: 1 if successful, -1 if otherwise
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
