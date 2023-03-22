4238088929677523f70034f2d6d9f8a247932e82
Oluwaremilekun <simplyremmy@gmail.com>
2023-03-22 11:09:49 -0700

    a fizzbuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints numbers from 1 - 100
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
